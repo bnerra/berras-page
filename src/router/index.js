31b2e661f71abcd92a4927040812ae3003cf2a6e
CORPORATE\njb1019 <nickolas.berra@daugherty.com>
2018-11-26 16:27:59 -0600

    Format structure updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Resume from './views/Resume.vue'
-import Blog from './views/Blog.vue'
-import Contact from './views/Contact.vue'
+import Home from './pages/HomeView.vue'
+import Resume from './pages/ResumeView.vue'
+import Blog from './pages/BlogView.vue'
+import Contact from './pages/ContactView.vue'
 
 Vue.use(Router)
 
@@ -22,7 +22,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ './pages/AboutView.vue')
     },
     {
       path: '/resume',
